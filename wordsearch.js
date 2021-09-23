2293dbeacd77101026055987b00cd7361bdc37ba
Astarina Maulida <astarina.maulida@gmail.com>
2021-09-22 21:39:57 -0400

    Fix lint

--- wordsearch.js
+++ wordsearch.js
@@ -1,32 +1,32 @@
-const transpose = function (matrix) {
-    // Push new array inside
-    let array = [];
-    for (let i = 0; i < matrix[0].length; i++) {
-      array.push([]);
-    }
-  
-    for (let row = 0; row < matrix.length; row++) {
-      for (let col = 0; col < matrix[row].length; col++) {
-        array[col][row] = matrix[row][col] //change the col row to row col 
-      }
-    }
-    return array;
+const transpose = function(matrix) {
+  // Push new array inside
+  let array = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    array.push([]);
   }
 
-const wordSearch = (letters, word) => {
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) {
-            return true;
-        }
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[row].length; col++) {
+      array[col][row] = matrix[row][col]; //change the col row to row col
     }
-    const verticalJoin = transpose(letters).map(ls => ls.join(''))
-    for (l of verticalJoin) {
-        if (l.includes(word)) {
-            return true;
-        }
-    }
-    return false;
-}
+  }
+  return array;
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  for (let l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+module.exports = wordSearch;
